d18a3628556d55905685eedfb8095aee4cd571dc
Pavan <pavan.kamatagi@gmail.com>
2021-11-01 19:05:32 +0530

    Added first .c file for the C-projects

---
+++ Introduction/Introduction.c
@@ -0,0 +1,23 @@
+/**************************************
+ * Basic Hello world like program
+ *
+ * @author : https://github.com/Pavanbk7
+ *
+***************************************/
+
+#include<stdio.h>
+
+
+/***************************************
+ * @main
+ *
+***************************************/
+int main()
+{
+    /* Basic Hello world like program */
+    printf("Hello there! welcome C-Projects Git-hub repository\n");
+}
+/***************************************
+ * end
+ *
+***************************************/
